ea47fadcb4c79a6fe29aab8e4043952463ab4715
Timothy Lock <lock.k.timothy@gmail.com>
2019-01-19 03:40:29 -0500

    Remove if statement from takepic

--- onboard/takepic.sh
+++ onboard/takepic.sh
@@ -1,7 +1,2 @@
-unamestr=`uname`
 fname=$1
-if [[ "$unamestr" == 'Linux' ]]; then
-   fswebcam -d /dev/video2 -r 1280x720 $fname
-else
-   imagesnap $fname
-fi
+fswebcam -d /dev/video2 -r 1280x720 $fname
